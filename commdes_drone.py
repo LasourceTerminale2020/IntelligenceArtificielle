3ba320c09559a8743ce745c972077348abfa5b3c
abelmaxv <59065710+abelmaxv@users.noreply.github.com>
2020-03-12 19:24:27 +0100

    Update commdes_drone.py

--- commdes_drone.py
+++ commdes_drone.py
@@ -15,7 +15,7 @@ while run:
 
     keys= pygame.key.get_pressed()
     
-    if keys [pygame.K_T]:
+    if keys [pygame.K_t]:
         run= False
     
     if keys [pygame.K_LEFT]:
@@ -30,16 +30,16 @@ while run:
     if keys[pygame.K_DOWN]:
         my_drone.back(vel)
 
-    if keys [pygame.K_Z]:
+    if keys [pygame.K_z]:
         my_drone.up(vel)
 
-    if keys [pygame.K_S]:
+    if keys [pygame.K_s]:
         my_drone.down(vel)
 
-    if keys [pygame.K_Q]:
+    if keys [pygame.K_q]:
         my_drone.ccw (vel)
     
-    if keys [pygame.K_D]:
+    if keys [pygame.K_d]:
         my_drone.cw (vel)
 
 
